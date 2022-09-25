1dc5c3ed9d5bd6f699281e2857852e28a33baf67
LikimiaD <somethingforsoftware@gmail.com>
2022-09-25 19:46:38 +0300

    25/09/2022

---
+++ СSharp/Algorithms/Mini-max_sum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long? min = null;
+        long? max = null;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            long num = -arr[i];
+            foreach (var item in arr)
+            {
+                num += item;
+            }
+            if (max < num || max == null)
+                max = num;
+            if (min > num || min == null)
+                min = num;
+        }
+        Console.WriteLine($"{min} {max}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
---
+++ СSharp/Algorithms/plus_minus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        var plus = 0;
+        var minus = 0;
+        var zero = 0;
+        foreach (var item in arr)
+        {
+            if (item > 0)
+                plus++;
+            if (item < 0)
+                minus++;
+            if (item == 0)
+                zero++;
+        }
+        Console.WriteLine($"{Math.Round((double)plus / (double)arr.Count, 5)}\n{Math.Round((double)minus / (double)arr.Count, 5)}\n{Math.Round((double)zero / (double)arr.Count, 5)}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
